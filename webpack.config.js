2e653364d263916255644f5430be05d614f16fe1
danilobrinu <ddbn.c2@gmail.com>
2019-04-14 19:37:36 -0500

    Init

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = (_, { mode = 'development' }) => ({
+	entry: {
+		app: './src/index.js',
+	},
+
+	devServer: {
+		contentBase: './dist',
+		compress: true,
+		port: 3000,
+		historyApiFallback: true,
+		disableHostCheck: true,
+	},
+
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'Development',
+			template: './src/index.pug',
+		}),
+	],
+
+	output: {
+		filename: '[name].[hash].bundle.js',
+		chunkFilename: '[name].[contenthash].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.pug$/,
+        include: /(index)\.pug$/,
+        use: 'pug-loader'
+      },
+      {
+				test: /\.pug$/,
+				exclude: /(index|404)\.pug$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name]-[hash].html',
+						},
+					},
+					'extract-loader',
+					'html-loader',
+					'pug-html-loader',
+				],
+			},
+		],
+	},
+
+	resolve: {
+		modules: ['node_modules'],
+		extensions: ['.js', '.scss', '.pug'],
+	},
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					priority: -10,
+					enforce: true,
+					chunks: 'all',
+				},
+			},
+		},
+	},
+});
