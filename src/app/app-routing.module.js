1a70f4574e524bf2dda31026667063680e6ccd8d
danilobrinu <ddbn.c2@gmail.com>
2019-04-20 16:32:31 -0500

    Add app routing module

---
+++ src/app/app-routing.module.js
@@ -0,0 +1,33 @@
+import angular from 'angular';
+import ocLazyLoad from 'oclazyload';
+import uiRouter from '@uirouter/angularjs';
+
+export const AppRoutingModule = angular
+	.module('app.routing', [uiRouter, ocLazyLoad])
+	.config([
+		'$stateProvider',
+		'$urlServiceProvider',
+		($stateProvider, $urlServiceProvider) => {
+			$urlServiceProvider.rules.otherwise('/error/404');
+			$stateProvider.state('error', {
+				url: '/error/:type',
+				component: 'errorPage',
+				lazyLoad: (transition) => {
+					const $ocLazyLoad = transition.injector().get('$ocLazyLoad');
+					return import('@components/pages/error-page/error-page.module').then(
+						(mod) => $ocLazyLoad.inject(mod.ErrorPageModule)
+					);
+				},
+			});
+			$stateProvider.state('notfound', {
+				url: '/404',
+				component: 'notFoundPage',
+				lazyLoad: (transition) => {
+					const $ocLazyLoad = transition.injector().get('$ocLazyLoad');
+					return import('@components/pages/not-found-page/not-found-page.module').then(
+						(mod) => $ocLazyLoad.inject(mod.NotFoundPageModule)
+					);
+				},
+			});
+		},
+	]).name;
