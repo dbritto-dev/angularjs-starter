9daebebf68b8de5668d58447c81242cfa7c1c326
danilobrinu <ddbn.c2@gmail.com>
2019-04-22 09:45:21 -0500

    Add home-page component

---
+++ src/components/pages/home-page/home-page.component.js
@@ -0,0 +1,17 @@
+import HomePageTemplate from './home-page.component.pug';
+
+class HomePageController {
+	static $inject = ['__INITIAL_STATE__'];
+
+	constructor(__INITIAL_STATE__) {
+		this.__INITIAL_STATE__ = __INITIAL_STATE__;
+	}
+
+	$onInit() {}
+}
+
+export const HomePageComponent = {
+	bindings: {},
+	templateUrl: HomePageTemplate,
+	controller: HomePageController,
+};
