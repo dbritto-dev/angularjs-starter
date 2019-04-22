f5f3ecfec8e472642e8022ad6db1b31b5931155f
danilobrinu <ddbn.c2@gmail.com>
2019-04-22 09:46:23 -0500

    Add home page module

---
+++ src/components/pages/home-page/home-page.module.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+import { HomePageComponent } from './home-page.component';
+
+export const HomePageModule = angular
+	.module('components.pages.error-page', [])
+  .component('homePage', HomePageComponent)
+  .name;
