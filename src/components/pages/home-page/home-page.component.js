f0c34e9f7b174ee9b1bcbdcbd1d667818fbe883c
danilobrinu <ddbn.c2@gmail.com>
2019-06-10 21:35:43 -0500

    Update home page component

--- src/components/pages/home-page/home-page.component.js
+++ src/components/pages/home-page/home-page.component.js
@@ -1,17 +1,17 @@
 import HomePageTemplate from './home-page.component.pug';
 
 class HomePageController {
-	static $inject = ['__INITIAL_STATE__'];
+  static $inject = ['__INITIAL_STATE__'];
 
-	constructor(__INITIAL_STATE__) {
-		this.__INITIAL_STATE__ = __INITIAL_STATE__;
-	}
+  constructor(__INITIAL_STATE__) {
+    this.__INITIAL_STATE__ = __INITIAL_STATE__;
+  }
 
-	$onInit() {}
+  $onInit() {}
 }
 
 export const HomePageComponent = {
-	bindings: {},
-	templateUrl: HomePageTemplate,
-	controller: HomePageController,
+  bindings: {},
+  templateUrl: HomePageTemplate,
+  controller: HomePageController,
 };
