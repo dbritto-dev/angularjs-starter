71b1f3e332d03089aed5dfd9ca5a720accfe1097
danilobrinu <ddbn.c2@gmail.com>
2019-06-10 21:35:31 -0500

    Update error page module

--- src/components/pages/error-page/error-page.module.js
+++ src/components/pages/error-page/error-page.module.js
@@ -2,6 +2,5 @@ import angular from 'angular';
 import { ErrorPageComponent } from './error-page.component';
 
 export const ErrorPageModule = angular
-	.module('components.pages.error-page', [])
-  .component('errorPage', ErrorPageComponent)
-  .name;
+  .module('components.pages.error-page', [])
+  .component('errorPage', ErrorPageComponent).name;
